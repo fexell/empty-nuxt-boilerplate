bc2d97a2de6bb34a6fb783e2edb49fd83ac0a4fa
FelixLocal <fexellofficial@gmail.com>
2024-10-14 10:37:00 +0200

    Initial commit

---
+++ components/Layout/Content/Content.component.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+  <section
+    id='Content'
+    class='content component flex flex-col flex-1'>
+    <div class='content-container component-container flex flex-col flex-1 min-h-full'>
+      <NuxtPage />
+    </div>
+  </section>
+</template>
